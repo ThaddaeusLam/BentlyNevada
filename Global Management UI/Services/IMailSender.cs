747a6c14856c22c1c9397afc0e2d6ef01b294856
MasterCal <88394390+MasterCal@users.noreply.github.com>
2023-04-26 12:30:37 -0700

    Update IMailSender.cs

--- Global Management UI/Services/IMailSender.cs
+++ Global Management UI/Services/IMailSender.cs
@@ -1,1 +1,11 @@
+//Code written by Caleb Stickler
 
+using System.Threading.Tasks;
+
+namespace Global_Management_UI.Services
+{
+    public interface IMailSender
+    {
+        Task SendEmailAsync(string appuser, string email, string subject, string HtmlMessage);
+    }
+}
